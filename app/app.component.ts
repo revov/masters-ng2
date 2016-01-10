2d6d221172faaefa3507702435e8e6cfa99c42fe
Stoyan Revov <st.revov@gmail.com>
2016-01-10 18:45:20 +0200

    Add skeleton girhubBrowser component

---
+++ app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, View } from 'angular2/core';
+import { GithubBrowser } from './githubBrowser.component';
+
+@Component({
+    selector: 'main-app'
+})
+@View({
+    template: `
+    <div class="ui center aligned padded page grid">
+        <div class="column">
+            <h2 class="ui teal header">
+                <div class="content">
+                    Github браузър
+                </div>
+            </h2>
+            <div class="ui large form">
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="github icon"></i>
+                        <input type="text" name="repo" placeholder="Хранилище">
+                    </div>
+                </div>
+
+                <div class="ui fluid large teal submit button">Намери</div>
+            </div>
+
+            <div class="ui divider"></div>
+
+            <div class="container">
+                <github-browser></github-browser>
+            </div>
+        </div>
+
+    </div>
+    `,
+    directives: [GithubBrowser]
+})
+export class App { }
--- app/main.ts
+++ app/main.ts
@@ -2,26 +2,8 @@ import 'reflect-metadata';
 import 'es6-shim';
 import 'semantic-ui';
 
-import { Component, View } from 'angular2/core';
-
 import { bootstrap } from 'angular2/platform/browser';
-
-//create a simple angular component
-@Component({
-  selector: 'main-app'
-})
-@View({
-  template: '<h4>Hello {{name}}</h4>'
-})
-class Main {
-  name: string;
-  constructor(){
-    this.name = "sample";
-    setTimeout(() => {
-      this.name = 'Angular2!!!'
-    },1500);
-  }
-}
+import { App } from './app.component';
 
 //start our app
-bootstrap(Main);
+bootstrap(App);
